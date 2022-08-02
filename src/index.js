133e3de60c4aaac2023aa44218a37b0e72e03dcb
Elvis <elvisnipah@gmail.com>
2022-08-02 13:35:11 +0000

    filter needed info from API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './style.css';
 
 async function callAPI(location) {
-  let apiData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3d7a4b39c8f2786fe333b4d256ce0134`);
+  let apiData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3d7a4b39c8f2786fe333b4d256ce0134&units=metric`);
   let jsonData = await apiData.json();
-  console.log(jsonData);
+  let finalData = processData(jsonData);
+  console.log(finalData);
 };
+
+function processData(data) {
+  let processedData = {
+    coordinates: data.coord,
+    main: data.main,
+    name: data.name,
+    weather: data.weather,
+    wind: data.wind
+  };
+  return processedData;
+}
 
 callAPI("Accra");
