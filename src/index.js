1fc2c2894d43c28be499f638589a5b0e0688092b
Elvis <elvisnipah@gmail.com>
2022-08-02 12:56:04 +0000

    call api and log info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+async function callAPI(location) {
+  let apiData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3d7a4b39c8f2786fe333b4d256ce0134`);
+  let jsonData = await apiData.json();
+  console.log(jsonData);
+};
+
+callAPI("Accra");
