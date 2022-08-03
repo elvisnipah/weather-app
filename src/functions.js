03e519cbf83646661d89372fc9540ac6632bb093
Elvis <elvisnipah@gmail.com>
2022-08-03 13:30:36 +0000

    remove console.log

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,6 @@ async function callAPI(location) {
   let apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=3d7a4b39c8f2786fe333b4d256ce0134&units=metric`);
   let jsonData = await apiData.json();
   let finalData = processData(jsonData);
-  console.log(finalData);
   return finalData;
 };
 
